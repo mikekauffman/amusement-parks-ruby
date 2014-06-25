18dfce9b8b985928f36bb6eb69bbe8e6a13f0d7a
mikekauffman <mike.a.kauffman@gmail.com>
2014-06-25 09:24:16 -0600

    amusement park data is indexed by country

--- lib/indexer.rb
+++ lib/indexer.rb
@@ -1,8 +1,10 @@
 class Indexer
+
   def initialize(input)
     @input = input
   end
-  def sort_by_id
+
+  def index_by_id
     result = {}
     @input.each do |park_data|
       result[park_data[:id]] = {
@@ -15,37 +17,22 @@ class Indexer
     end
     result
   end
+
+  def index_by_country
+    result = {}
+    @input.each do |park_data|
+      country = park_data[:country]
+      parks_info_array = result.fetch(country) { [] }
+      parks_info_array = [] if parks_info_array.nil?
+      park_info_hash = {
+        id: park_data[:id],
+        name: park_data[:name],
+        city: park_data[:city],
+        state: park_data[:state],
+        country: park_data[:country],
+      }
+      result[country] = parks_info_array << park_info_hash
+    end
+    result
+  end
 end
-#
-# input = [
-#   {
-#     :id=>546,
-#     :name=>"Kalahari Resorts",
-#     :city=>"Wisconsin Dells",
-#     :state=>"Wisconsin",
-#     :country=>"United States"
-#   },
-#   {
-#     :id=>547,
-#     :name=>"Little Amerricka",
-#     :city=>"Marshall",
-#     :state=>"Wisconsin",
-#     :country=>"United States"
-#   }
-# ]
-# expected = {
-#   546 => {
-#     :id=>546,
-#     :name=>"Kalahari Resorts",
-#     :city=>"Wisconsin Dells",
-#     :state=>"Wisconsin",
-#     :country=>"United States"
-#   },
-#   547 => {
-#     :id=>547,
-#     :name=>"Little Amerricka",
-#     :city=>"Marshall",
-#     :state=>"Wisconsin",
-#     :country=>"United States"
-#   }
-# }
--- spec/indexer_spec.rb
+++ spec/indexer_spec.rb
@@ -36,6 +36,60 @@ describe Indexer do
       }
     }
     indexer = Indexer.new(input)
-    expect(indexer.sort_by_id).to eq(expected)
+    expect(indexer.index_by_id).to eq(expected)
+  end
+  it 'indexes amusement park data by country' do
+    input = [
+      {
+        :id=>546,
+        :name=>"Kalahari Resorts",
+        :city=>"Wisconsin Dells",
+        :state=>"Wisconsin",
+        :country=>"United States"
+      },
+      {
+        :id=>547,
+        :name=>"Little Amerricka",
+        :city=>"Marshall",
+        :state=>"Wisconsin",
+        :country=>"United States"
+      },
+      {
+        :id=>2,
+        :name=>"Calaway Park",
+        :city=>"Calgary",
+        :state=>"Alberta",
+        :country=>"Canada"
+      }
+    ]
+    expected = {
+      "Canada" => [
+        {
+          :id=>2,
+          :name=>"Calaway Park",
+          :city=>"Calgary",
+          :state=>"Alberta",
+          :country=>"Canada"
+        }
+      ],
+      "United States" => [
+        {
+          :id=>546,
+          :name=>"Kalahari Resorts",
+          :city=>"Wisconsin Dells",
+          :state=>"Wisconsin",
+          :country=>"United States"
+        },
+        {
+          :id=>547,
+          :name=>"Little Amerricka",
+          :city=>"Marshall",
+          :state=>"Wisconsin",
+          :country=>"United States"
+        }
+      ]
+    }
+    indexer = Indexer.new(input)
+    expect(indexer.index_by_country).to eq(expected)
   end
 end
