79ab732e7f2620a4d637e512c20b685de6949b28
mikekauffman <mike.a.kauffman@gmail.com>
2014-06-25 09:31:04 -0600

    amusement park data is indexed by country and state

--- lib/indexer.rb
+++ lib/indexer.rb
@@ -12,7 +12,7 @@ class Indexer
         name: park_data[:name],
         city: park_data[:city],
         state: park_data[:state],
-        country: park_data[:country],
+        country: park_data[:country]
       }
     end
     result
@@ -29,9 +29,27 @@ class Indexer
         name: park_data[:name],
         city: park_data[:city],
         state: park_data[:state],
-        country: park_data[:country],
+        country: park_data[:country]
       }
       result[country] = parks_info_array << park_info_hash
+    end
+    result
+  end
+
+  def index_by_country_and_state
+    result = {}
+    @input.each do |park_data|
+      state_and_country = "#{park_data[:state]}, #{park_data[:country]}"
+      parks_info_array = result.fetch(state_and_country) { [] }
+      parks_info_array = [] if parks_info_array.nil?
+      parks_info_hash = {
+        id: park_data[:id],
+        name: park_data[:name],
+        city: park_data[:city],
+        state: park_data[:state],
+        country: park_data[:country]
+      }
+      result[state_and_country] =  parks_info_array << parks_info_hash
     end
     result
   end
--- spec/indexer_spec.rb
+++ spec/indexer_spec.rb
@@ -92,4 +92,74 @@ describe Indexer do
     indexer = Indexer.new(input)
     expect(indexer.index_by_country).to eq(expected)
   end
+  it 'indexes by country and state' do
+    input = [
+      {
+        :id=>3,
+        :name=>"Galaxyland",
+        :city=>"Edmonton",
+        :state=>"Alberta",
+        :country=>"Canada"
+      },
+      {
+        :id=>4,
+        :name=>"Heratage Park",
+        :city=>"Calgary",
+        :state=>"Alberta",
+        :country=>"Canada"
+      },
+      {
+        :id=>6,
+        :name=>"Playland (Vancouver)",
+        :city=>"Vancouver",
+        :state=>"British Columbia",
+        :country=>"Canada"
+      },
+      {
+        :id=>8,
+        :name=>"Crystal Palace Amusement Park",
+        :city=>"Dieppe",
+        :state=>"New Brunswick",
+        :country=>"Canada"
+      }
+    ]
+    expected = {
+      "Alberta, Canada" => [
+        {
+          :id => 3,
+          :name => "Galaxyland",
+          :city => "Edmonton",
+          :state => "Alberta",
+          :country => "Canada"
+        },
+        {
+          :id => 4,
+          :name => "Heratage Park",
+          :city => "Calgary",
+          :state => "Alberta",
+          :country => "Canada"
+        },
+      ],
+      "British Columbia, Canada" => [
+        {
+          :id => 6,
+          :name => "Playland (Vancouver)",
+          :city => "Vancouver",
+          :state => "British Columbia",
+          :country => "Canada"
+        },
+      ],
+      "New Brunswick, Canada" => [
+        {
+          :id => 8,
+          :name => "Crystal Palace Amusement Park",
+          :city => "Dieppe",
+          :state => "New Brunswick",
+          :country => "Canada"
+        }
+      ]
+    }
+    indexer = Indexer.new(input)
+    expect(indexer.index_by_country_and_state).to eq(expected)
+  end
 end
